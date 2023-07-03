ffac8eccd14beee16e99d87e2b73a06d15cf3e0c
Timothy Remst <11875728+tjaynl@users.noreply.github.com>
2023-07-03 16:15:07 +0200

    First commit
    This is the first upload

---
+++ _config.ts
@@ -0,0 +1,44 @@
+import lume from "lume/mod.ts";
+import metas from "lume/plugins/metas.ts";
+import inline from "lume/plugins/inline.ts";
+import attributes from "lume/plugins/attributes.ts";
+import imagick from "lume/plugins/imagick.ts";
+import netlify_cms from "lume/plugins/netlify_cms.ts";
+import postcss from "lume/plugins/postcss.ts";
+import sass from "lume/plugins/sass.ts";
+import svgo from "lume/plugins/svgo.ts";
+import esbuild from "lume/plugins/esbuild.ts";
+
+const site = lume({
+    server: {
+        open: true,
+    },
+    src: "./_src",
+});
+
+site.use(attributes())
+    .use(inline())
+    .use(metas())
+    .use(imagick())
+    .use(sass())
+    .use(postcss())
+    .use(svgo())
+    .use(esbuild({
+        extensions: [".ts", ".js"],
+        options: {
+            bundle: true,
+            format: "esm",
+            minify: true,
+            keepNames: true,
+            platform: "browser",
+            target: "esnext",
+            incremental: true,
+            treeShaking: true,
+        },
+    }))
+    .use(netlifyCMS());
+
+site.copy("static", ".")
+    .copy("js");
+
+export default site;
